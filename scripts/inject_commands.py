3ffc0de0e22f1575f0d135e42066a3ecaf42d323
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-11-14 20:30:06 -0500

    Smarter injecting of commands in relation to peer up/down events

--- scripts/inject_commands.py
+++ scripts/inject_commands.py
@@ -15,23 +15,67 @@ def main():
             required=True, help='Path for BMP message stream')
     arg_parser.add_argument('-output', dest='output_path', action='store',
             required=True, help='Path for modified message stream')
+    arg_parser.add_argument('-rdns', dest='rdns_path', action='store',
+            default=None, help='Path to rdns JSON file')
     arg_parser.add_argument('-peerchange', dest='peerchange',
             action='store_true', 
             help='Print and reset network summary on every peer up/down event')
     settings = arg_parser.parse_args()
 
+    # Load rdns
+    rdns = {}
+    if settings.rdns_path is not None:
+        with open(settings.rdns_path, 'r') as rdnsfile:
+            rdns = nopticon.parse_rdns(rdnsfile.read())
+
     # Open input and output streams
     istream = open(settings.input_path, 'r')
     ostream = open(settings.output_path, 'w')
 
+    # Modification-specific variables
+    if (settings.peerchange):
+        seen_peer_down = False
+        up_links = []
+        down_links = []
+
     # Process input stream
     for bmp_json in istream.readlines():
         bmp_msg = bmp.parse_message(bmp_json.strip())
-        if (settings.peerchange 
-                and (bmp_msg.isPeerUp() or bmp_msg.isPeerDown())):
-            ostream.write(nopticon.Command.print_log().json()+'\n')
-            refresh = nopticon.Command.refresh_summary(bmp_msg._timestamp)
-            ostream.write(refresh.json()+'\n')
+        if (settings.peerchange and
+                (bmp_msg.isPeerUp() or bmp_msg.isPeerDown())): 
+
+            # Special flag to avoid inserting commands when network is still 
+            # starting
+            if (bmp_msg.isPeerDown()):
+                seen_peer_down = True
+
+            # Determine if link went up or down
+            edge = bmp_msg.edge()
+            if settings.rdns_path is not None:
+                assert edge[0] in rdns, "%s not in rdns" % edge[0]
+                assert edge[1] in rdns, "%s not in rdns" % edge[1]
+                edge = (rdns[edge[0]], rdns[edge[1]])
+            if (edge[1] < edge[0]):
+                edge = (edge[1], edge[0])
+            change = False
+            if (bmp_msg.isPeerUp() and edge not in up_links):
+                print('Up %s-%s' % edge)
+                up_links.append(edge)
+                if (edge in down_links):
+                    down_links.remove(edge)
+                change = True
+            elif (bmp_msg.isPeerDown() and edge not in down_links):
+                print('Down %s-%s' % edge)
+                down_links.append(edge)
+                if (edge in up_links):
+                    up_links.remove(edge)
+                change = True
+
+            # Write commands if a link changed state
+            if (seen_peer_down and change):
+                ostream.write(nopticon.Command.print_log().json()+'\n')
+                refresh = nopticon.Command.refresh_summary(bmp_msg._timestamp)
+                ostream.write(refresh.json()+'\n')
         ostream.write(bmp_json)
 
     # Close input and output streams
